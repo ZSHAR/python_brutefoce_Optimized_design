66991e4db55d36c11a1966ba8303459de29dfef7
ZSHAR <94743009+ZSHAR@users.noreply.github.com>
2026-06-20 19:58:26 +0530

    Add files via upload

--- Day_17_Quiz_Game/my_solution/main.py
+++ Day_17_Quiz_Game/my_solution/main.py
@@ -1,1 +1,19 @@
-print("Hello")
+from question_model import Question
+from data import question_data
+question_bank=[]
+for question in question_data:
+    question_text=question["question"]
+    question_answer=question["correct_answer"]
+    new_question=Question(question_text,question_answer)
+    question_bank.append(new_question)
+score=0
+count=0
+for question in question_bank:
+    answer=input(f"{question.question}True or False: ").lower()
+    count+=1
+    if answer==question.answer.lower():
+        print("Your answer is correct!")
+        score+=1
+    else:
+        print("Your answer is incorrect!")
+    print(f"Your score is {score}/{count}")
